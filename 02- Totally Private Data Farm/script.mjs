3cca5298884bd49484d848457193647f95cabf37
Amol shelke <shelkeamol897@gmail.com>
2022-12-05 23:57:37 +0530

    Working on solution of the challenge 2

---
+++ 02- Totally Private Data Farm/script.mjs
@@ -0,0 +1,23 @@
+/*private user data! 
+
+We'd never do this in real life of course, but just for practice 
+let's pretend we're unethical web hackers and transform the data 
+in the way Evil Corp. has requested. They're quite particular and
+just want an array of users with a fullname and human readable
+birthday.   
+
+Write a function that maps through the current data and returns
+a new an array of objects with only two properties: 
+fullName and birthday. Each result in your 
+array should look like this when you're done: 
+
+{
+    fullName: "Levent Busser", 
+    birthday: "Fri Aug 20 1971"
+}
+
+Read about toDateString() for info on formatting a readable date. 
+
+*/
+
+import { users } from "./data";
