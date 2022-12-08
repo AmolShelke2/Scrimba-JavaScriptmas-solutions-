eb88d886b576bc67f5d790fa99796e96a80a3c7c
Amol shelke <shelkeamol897@gmail.com>
2022-12-08 23:54:05 +0530

    Added solution of the 06- Taco Tray Challenge

---
+++ 06- Taco Tray/script.js
@@ -0,0 +1,26 @@
+// Review Challenge 4: Taco Tray
+// - Help our chef fill a tray with tacos!
+
+function getRandomNumberOfTacos() {
+  /*
+    Make this function return an array that contains 
+    between one and ten taco emojis 🌮
+    Use the following JavaScript concepts:
+        - Math.random()
+        - Math.floor()
+        - new Array()
+        - Array.fill()
+    */
+
+  return ["Empty Tray"]; // replace this empty tray array
+}
+
+function putTacosOnTray() {
+  return getRandomNumberOfTacos()
+    .map(function (taco) {
+      return `<div class="taco">${taco}</div>`;
+    })
+    .join("");
+}
+
+document.getElementById("tray").innerHTML = putTacosOnTray();
