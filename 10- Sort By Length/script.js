447d77ae03221a1a79a89eb3250b026696353f56
Amol shelke <shelkeamol897@gmail.com>
2022-12-10 22:13:04 +0530

    Adding solution of the 10- Sort by length challenge

---
+++ 10- Sort By Length/script.js
@@ -0,0 +1,11 @@
+/*  given an array of strings, sort them in the order of increasing lengths. if two strings 
+have the same their relative order must be the inital order
+
+Example: 
+
+For
+inputArray = ["abc", "", "aaa", "a", "zz"]
+
+the output should be 
+sortByLength(inputArray) = ["", "a", "zz", "abc", "aaa"]
+*/
