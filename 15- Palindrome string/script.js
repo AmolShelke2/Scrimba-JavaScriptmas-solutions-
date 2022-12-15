8b753018de82bc9adb75c451fedfe8ccc8908441
Amol shelke <shelkeamol897@gmail.com>
2022-12-15 22:33:59 +0530

    Added solution of 15- Palindrom string

---
+++ 15- Palindrome string/script.js
@@ -0,0 +1,19 @@
+/* 
+Palindromes are words that are the same forward or backward. For example, 
+the words "noon" and "kayak" are a palindromes.
+ 
+Write a function to check if a lowercased string of letters is a palindrome. 
+If the word is palidrome, return true. If it isn't, return false. 
+*/
+
+function isPalindrome(str) {
+  const palindromeStr = str.split("").reverse().join("");
+  return palindromeStr === str ? true : false;
+}
+
+// Test your function
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
