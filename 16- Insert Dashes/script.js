b23c570b2dd39244b97cc011445313b50c379794
Amol shelke <shelkeamol897@gmail.com>
2022-12-16 23:49:20 +0530

    Added Problem statement

--- 16- Insert Dashes/script.js
+++ 16- Insert Dashes/script.js
@@ -1,3 +1,10 @@
+/*
+Transform a given sentence into a new one with dashes between each two consecutive letters
+
+Example
+for inputString = 'abc caba' the output should be insertDashes(inputString) = 'a-b-c c-a-b-a'
+*/
+
 function insertDashes(arr) {
   // write code here
   return arr
