99c1c6b610e4e5afb3879f85d2ea4fc2a1cb1025
Amol shelke <shelkeamol897@gmail.com>
2022-12-18 14:21:49 +0530

    Adding challenge 18- Candies

---
+++ 18- Candies/script.js
@@ -0,0 +1,10 @@
+/*
+n children have got m pieces of candy. They want to eat much candy as they can, but each 
+child must eat exactly the same amount of candy as any other child. Determine how many pieces 
+of candy will be eaten by all the children together. Individual pieces of candy cannot be split.
+
+// Example
+for n = 3 and m = 10, then the output sholud be candies(n, m) = 9.
+Each child will eat 3 pieces, So the answer is 9
+
+*/
