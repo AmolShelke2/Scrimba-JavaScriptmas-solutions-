d3d4373832700920ffec8cb43f56093fa305544d
Amol shelke <shelkeamol897@gmail.com>
2022-12-19 23:24:40 +0530

    Added challenge statement

---
+++ 19- Century From Year/script.js
@@ -0,0 +1,8 @@
+/*
+Given a year, return the century it is in, The first century spans from the year 1 up to 
+including the year 100, the second from the year 101 up to and including the year 200 etc.
+
+Example: 
+For year = 1905 the output should be centuryFromYear(year) = 20
+
+*/
