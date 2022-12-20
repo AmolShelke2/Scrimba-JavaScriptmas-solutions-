898f47057c8bc7d18d2cdc770182f144f714b177
Amol shelke <shelkeamol897@gmail.com>
2022-12-20 21:28:52 +0530

    Added Challenge statement

---
+++ 20- Find Free Prodcasts/script.js
@@ -0,0 +1,19 @@
+/* Find Free Podcasts 
+
+We have a list of podcasts and need the ability to filter by only
+podcasts which are free.
+
+Write a function that takes in the podcast data and returns an new
+array of only those podcasts which are free.
+
+Additionally, your new array should return only 
+objects containing only the podcast title, rating, and whether or 
+not it is paid. 
+
+Expected output: 
+[
+    {title: "Scrimba Podcast", rating: 10, paid: false}, 
+    {title: "Something about Witches", rating: 8, paid: false}, 
+    {title: "Coding Corner", rating: 9, paid: false}
+]
+*/
