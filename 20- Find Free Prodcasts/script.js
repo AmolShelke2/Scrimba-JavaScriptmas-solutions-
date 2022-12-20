7b9829b2702118db954a414bbcc3eb6e9c87faf8
Amol shelke <shelkeamol897@gmail.com>
2022-12-20 22:14:16 +0530

    Added Final Solution for the 20- Find Free Prodcast

--- 20- Find Free Prodcasts/script.js
+++ 20- Find Free Prodcasts/script.js
@@ -9,7 +9,7 @@ array of only those podcasts which are free.
 Additionally, your new array should return only 
 objects containing only the podcast title, rating, and whether or 
 not it is paid. 
-
+ 
 Expected output: 
 [
     {title: "Scrimba Podcast", rating: 10, paid: false}, 
@@ -17,3 +17,76 @@ Expected output:
     {title: "Coding Corner", rating: 9, paid: false}
 ]
 */
+const prodcasts = [
+  {
+    id: 1,
+    title: "Scrimba Podcast",
+    duration: 50,
+    tags: ["education", "jobs", "technology"],
+    hosts: ["Alex Booker"],
+    rating: 10,
+    genre: "education",
+    paid: false,
+  },
+  {
+    id: 2,
+    title: "Crime Fan",
+    duration: 150,
+    tags: ["crime", "entertainment", "mature"],
+    hosts: ["Bob Smith", "Camilla Lambert"],
+    genre: "true crime",
+    rating: 5,
+    paid: true,
+  },
+  {
+    id: 3,
+    title: "Mythical Creatures",
+    duration: 99,
+    tags: ["entertainment", "general", "unicorns"],
+    hosts: ["Esmerelda Shelley", "Duke Dukington", "Felix the Cat"],
+    genre: "fantasy",
+    rating: 8,
+    paid: true,
+  },
+  {
+    title: "Crime Crime Crime",
+    duration: 70,
+    tags: ["crime", "entertainment", "mature"],
+    hosts: ["Jessica Jones", "Humphrey Bogart", "Inspector Gadget"],
+    genre: "true crime",
+    rating: 6,
+    paid: true,
+  },
+  {
+    title: "Something about Witches",
+    duration: 35,
+    tags: ["fantasy", "entertainment"],
+    hosts: ["Frewin Wyrm", "Evanora Highmore"],
+    genre: "fantasy",
+    rating: 8,
+    paid: false,
+  },
+  {
+    title: "Coding Corner",
+    duration: 55,
+    tags: ["education", "jobs", "technology"],
+    hosts: ["Treasure Porth", "Guil Hernandez", "Tom Chant"],
+    genre: "education",
+    rating: 9,
+    paid: false,
+  },
+];
+
+function getFreeProdcasts(data) {
+  return data
+    .map((prodcast) => {
+      return {
+        title: prodcast.title,
+        rating: prodcast.rating,
+        paid: prodcast.paid,
+      };
+    })
+    .filter((prodcast) => !prodcast.paid);
+}
+
+console.log(getFreeProdcasts(prodcasts));
