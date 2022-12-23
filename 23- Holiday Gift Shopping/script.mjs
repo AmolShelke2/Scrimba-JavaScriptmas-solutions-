0c51e23c9feb201d3d6a5c18f661b5192f08ad22
Amol shelke <shelkeamol897@gmail.com>
2022-12-23 23:18:38 +0530

    Added challenge staement and resource file

---
+++ 23- Holiday Gift Shopping/script.mjs
@@ -0,0 +1,18 @@
+/*
+    You're online shopping for holiday gifts, but money is tight
+    so we need to look at the cheapest items first. 
+    Use the built in sort() method to write a function that returns a new array of
+    products sorted by price, cheapest to most expensive. 
+    
+    Then log the item and the price to the console: 
+    
+    💕,0
+    🍬,0.89
+    🍫,0.99
+    🧁,0.99
+    📚,0.99
+    ... continued
+*/
+
+import products from "./data.js";
+console.log(products);
